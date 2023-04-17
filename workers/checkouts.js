75f2a5f644b9f0ebe097b682ace8554ea9f1db6f
Ben Peachey <potherca@gmail.com>
2023-04-17 17:24:48 +0200

    Change code for checkout worker
    - Restructure code so variable are before functions
    - Code cleanup
    - Place template in separate function
    - Improve error handling

--- workers/checkouts.js
+++ workers/checkouts.js
@@ -1,3 +1,29 @@
+const responseOptions = {
+  headers: {
+    "content-type": "text/html;charset=UTF-8"
+  },
+};
+
+const sizeMap = {
+  "extraextrasmallxxs": 'Extra Extra Small (XXS)',
+  "extrasmallxs": 'Extra Small (XS)',
+  "smalls": 'Small (S)',
+  "mediumm": 'Medium (M)',
+  "largel": 'Large (L)',
+  "extralargexl": 'Extra Large (XL)',
+  "extraextralargexxl": 'Extra Extra Large (XXL)',
+}
+
+const sizes = {
+  "extraextrasmallxxs": 0,
+  "extrasmallxs": 0,
+  "smalls": 0,
+  "mediumm": 0,
+  "largel": 0,
+  "extralargexl": 0,
+  "extraextralargexxl": 0,
+}
+
 async function fetchCheckouts(privateKey, starting_after = null) {
   const baseUrl = 'https://api.stripe.com/v1'
   let url = `${baseUrl}/checkout/sessions`
@@ -24,10 +50,10 @@ async function fetchCheckouts(privateKey, starting_after = null) {
     }
   })
   .then(async data => {
-    let checkouts = data.data.filter(checkout => checkout.status === 'complete');
+    let checkouts = data.data.filter(checkout => checkout.status === 'complete')
 
     if (data.has_more === true) {
-      const starting_after = data.data[data.data.length - 1].id;
+      const starting_after = data.data[data.data.length - 1].id
 
       const moreCheckouts = await fetchCheckouts(privateKey, starting_after)
 
@@ -38,34 +64,32 @@ async function fetchCheckouts(privateKey, starting_after = null) {
   })
 }
 
-const sizeMap = {
-  "extraextrasmallxxs": 'Extra Extra Small (XXS)',
-  "extrasmallxs": 'Extra Small (XS)',
-  "smalls": 'Small (S)',
-  "mediumm": 'Medium (M)',
-  "largel": 'Large (L)',
-  "extralargexl": 'Extra Large (XL)',
-  "extraextralargexxl": 'Extra Extra Large (XXL)',
-}
-
-const sizes = {
-  "extraextrasmallxxs": 0,
-  "extrasmallxs": 0,
-  "smalls": 0,
-  "mediumm": 0,
-  "largel": 0,
-  "extralargexl": 0,
-  "extraextralargexxl": 0,
-}
+const buildHtml = (title, content) => `<!doctype html>
+<html lang="">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Schooltrui VSZ</title>
+  <link rel="preconnect" href="https://cdn.jsdelivr.net">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css" integrity="sha256-rTpdO0HXBCNpreAHcu6tB2Ppg515Vo+5GtYSsnNLz+8=" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://schooltrui.nl/style.css">
+</head>
+<body>
+  <main class="container section">
+    <h1 class="title has-text-left is-1">Schooltrui VSZ</h1>
+    <section class="box section">
+      <h2 class="title">${title}</h2>
+      ${content}
+    </section>
+</body>
+</html>
+`
 
 export default {
   async fetch(request, env) {
-
-    let body = ''
-    const options = {}
-
-    options.headers = {"content-type": "text/html;charset=UTF-8"}
-
     return fetchCheckouts(env.stripe_token).then(checkouts =>
       checkouts.map(checkout => ({
         size: checkout.custom_fields[0].dropdown.value,
@@ -75,26 +99,13 @@ export default {
         sizes[order.size]++
       })
 
-      body += `<!doctype html>
-<html lang="">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Schooltrui VSZ</title>
+      let tableRows = ''
 
-  <link rel="preconnect" href="https://cdn.jsdelivr.net">
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css"
-    integrity="sha256-rTpdO0HXBCNpreAHcu6tB2Ppg515Vo+5GtYSsnNLz+8=" crossorigin="anonymous">
-  <link rel="stylesheet" href="https://schooltrui.nl/style.css">
-</head>
-<body>
-  <main class="container section">
-    <h1 class="title has-text-left is-1">Schooltrui VSZ</h1>
-    <section class="box section">
-      <h2 class="title">Bestellingen</h2>
+      Object.entries(sizes).forEach(([size, amount]) => {
+        tableRows += `<tr><td>${sizeMap[size]}</td><td>${amount}</td></tr>`
+      })
+
+      let table = `
       <table class="table is-striped">
         <thead>
         <tr>
@@ -109,42 +120,49 @@ export default {
         </tr>
         </tfoot>
         <tbody>
+        ${tableRows}
+        </tbody>
+      </table>
 `
-      Object.entries(sizes).forEach(([size, amount]) => {
-        body += `
-                  <tr>
-                    <td>${sizeMap[size]}</td>
-                    <td>${amount}</td>
-                  </tr>
-                `
-      })
-      body += `
-          </tbody>
-        </table>
-    </section>
-</body>
-</html>`
-      return new Response(body, options)
+
+      const body = buildHtml('Bestellingen', table)
+
+      return new Response(body, responseOptions)
     }).catch(async (error) => {
+      responseOptions.status = 500
+      responseOptions.statusText = '"Oh, bother." said Pooh, "Something has gone wrong."'
+
       if (typeof error.json === 'undefined') {
-        return new Response(error, {
-          status: 500
-        })
+        const body = buildHtml('Er ging iets mis!', `<pre>${error}</pre>`)
+        return new Response(body, responseOptions)
       } else {
         let json = {}
 
         try {
           json = await error.json()
-          json.url = url
         } catch (e) {
           json = error
         }
 
-        return new Response(JSON.stringify(json, null, 4), {
-          headers: error.headers || {},
-          status: error.status || 500,
-          statusText: error.statusText || 'Oh, bother.'
+        const body = buildHtml('Er ging iets mis!', `
+          <p>Neem contact op met de ontwikkelaar van de website, vermeld daarbij onderstaande foutmelding:</p>
+          <pre>${JSON.stringify(json, null, 4)}</pre>
+        `)
+
+        ['status', 'statusText', 'headers'].forEach(key => {
+          let value = error[key];
+
+          if (value !== undefined) {
+            if (key === 'headers') {
+              value = {}
+              error.headers.forEach((headerValue, headerKey) => {
+                value[headerKey] = headerValue
+              })
+            }
+            responseOptions[key] = value
+          }
         })
+        return new Response(body, responseOptions)
       }
     })
   }
